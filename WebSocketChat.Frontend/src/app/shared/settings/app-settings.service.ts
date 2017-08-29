75d1fd964602caf3d38aebf559ec1a2dec74287d
Pecral <pecral.dev@gmail.com>
2017-08-29 22:20:41 +0200

    Changed default message theme back previous setting

--- WebSocketChat.Frontend/src/app/shared/settings/app-settings.service.ts
+++ WebSocketChat.Frontend/src/app/shared/settings/app-settings.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
 export class AppSettingsService {
 
    applicationTheme = "light";
-   messageTheme = "irc";
+   messageTheme = "bubble";
    
    currentRoom: ChatRoom;
    chatIsActiveWindow:boolean = true;   
